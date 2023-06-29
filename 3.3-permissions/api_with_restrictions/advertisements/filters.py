73334a17551ffc26f1b2e2e8247a0556ebd6b1e9
Fedor Metsger <fedor.metsger@gmail.com>
2023-06-29 13:33:28 +0300

    Fix HW #3.3 defects

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,12 +1,12 @@
 from django_filters import rest_framework as filters
+from django_filters import FilterSet#, DateFilter
 
 from advertisements.models import Advertisement
 
-
-class AdvertisementFilter(filters.FilterSet):
-    """Фильтры для объявлений."""
-
-    # TODO: задайте требуемые фильтры
-
+class AdvertisementFilter(FilterSet):
+    created_at = filters.DateFromToRangeFilter()
+    # created_min = filters.DateFilter(field_name="created_at", lookup_expr='gte')
+    # created_max = filters.DateFilter(field_name="created_at", lookup_expr='lte')
     class Meta:
         model = Advertisement
+        fields = ["status", "created_at"]
